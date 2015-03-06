a44774c20832bb449b5e566628fb4f9725ed0d77
Suresh Alse <alseambusher@gmail.com>
2015-03-06 16:31:30 +0530

    1. base UI added 2. Base app constants added

---
+++ js/app.js
@@ -0,0 +1,18 @@
+// constants ---------------------------------------------
+
+// game state constants
+const var cGameStopped = 0;
+const var cGameStarted = 1;
+const var cGamePaused = 2;
+
+const var cMaxGameSpeed = 5;
+
+
+// globals -----------------------------------------------
+var G_GAME_SPEED = 1;
+var G_GAME_STATE = cGameStopped;
+
+
+$(document).ready(){
+	// TODO
+}
